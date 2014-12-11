a34fb08c6876792381851f5d6c89be149b948ae3
matthesrieke <m.rieke@52north.org>
2014-12-11 15:00:34 +0100

    geometry serialization test

--- 52n-ses-core/src/test/java/org/n52/ses/persistency/MapEventSerializationTest.java
+++ 52n-ses-core/src/test/java/org/n52/ses/persistency/MapEventSerializationTest.java
@@ -32,6 +32,10 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.n52.ses.api.event.MapEvent;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+
 public class MapEventSerializationTest {
 
 	@Test
@@ -40,6 +44,9 @@ public class MapEventSerializationTest {
 		me.put(MapEvent.ORIGNIAL_MESSAGE_KEY, XmlObject.Factory.parse("<test><content>123</content></test>"));
 		me.put(MapEvent.DOUBLE_VALUE_KEY, 23.5);
 		me.put(MapEvent.SENSORID_KEY, "theSensor");
+		GeometryFactory gf = new GeometryFactory();
+		Point geom = gf.createPoint(new Coordinate(52.1, 7.44112233));
+		me.put(MapEvent.GEOMETRY_KEY, geom);
 		
 		Object o = MapEvent.deserialize(new ByteArrayInputStream(me.serialize()));
 		
@@ -50,6 +57,7 @@ public class MapEventSerializationTest {
 		Assert.assertTrue(ome.get(MapEvent.SENSORID_KEY).equals("theSensor"));
 		Assert.assertTrue((Long) ome.get(MapEvent.START_KEY) == 2);
 		Assert.assertTrue((Long) ome.get(MapEvent.END_KEY) == 3);
+		Assert.assertTrue(((Point) ome.get(MapEvent.GEOMETRY_KEY)).equals(geom));
 	}
 	
 }
